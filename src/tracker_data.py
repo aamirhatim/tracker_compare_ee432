a408a40003b5d218fa31d541858c5bc183117c9e
Aamir Husain <aamir300@gmail.com>
2018-03-12 14:02:58 -0500

    Add ignore file. Edit MATLAB and tracker_data code to organize captured data better.

--- src/tracker_data.py
+++ src/tracker_data.py
@@ -9,6 +9,7 @@ def dir_exists(filename):
     name = filename[:-4]
     if not os.path.exists("../data/"+name):
         os.makedirs("../data/"+name)
+        os.makedirs("../data/"+name+"/img")
         print name,"folder added."
     return name
 
@@ -29,10 +30,10 @@ def get_center(bbox):
     y = int(bbox[1]+bbox[3]/2)
     return x, y
 
-def export_csv(name, tracker_type, t, x, y):
+def export_csv(name, tracker_type, t, x, y, fps):
     with open("../data/"+name+"/"+tracker_type+".csv", "a") as csvfile:
         filewriter = csv.writer(csvfile, delimiter = ",", quotechar = "|", quoting = csv.QUOTE_MINIMAL)
-        filewriter.writerow([t, x, y])
+        filewriter.writerow([t, x, y, fps])
 
 def draw_box(frame, bbox):
     p1 = (int(bbox[0]), int(bbox[1]))
@@ -42,15 +43,22 @@ def draw_box(frame, bbox):
 def draw_fail(frame):
     cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
-def display_tracker(frame, tracker_type, fps):
+def display_tracker(frame, frame_num, tracker_type, fps, name, count):
     cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
     cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-    cv2.imshow("Tracking", frame)
+    # cv2.imshow("Tracking", frame)
+
+    if count == 20:
+        cv2.imwrite("../data/"+name+"/img/"+tracker_type+"_"+str(frame_num)+".jpg", frame)
+        count = 0
+    else:
+        count += 1
 
     # Exit if ESC pressed
     k = cv2.waitKey(3) & 0xff
     if k == 27:
         return
+    return count
 
 def main():
     directory = "../video"                                      # Specify video directory
@@ -68,6 +76,8 @@ def main():
 
             for i in range(len(tracker_types)):
                 j = 0
+                count = 0
+                frame_num = 1
                 bbox = bounds                                   # Reset bounding box
                 tracker_type = tracker_types[i]
 
@@ -96,138 +106,19 @@ def main():
                     timer = cv2.getTickCount()                  # Start timer
                     ok, bbox = tracker.update(frame)            # Update tracker
 
-                    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);            # Get FPS
-                    t = int(((cv2.getTickCount() - time)/cv2.getTickFrequency())*1000)      # Get time elapsed
+                    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);                # Get FPS
+                    t = int(((cv2.getTickCount() - time)/cv2.getTickFrequency())*1000)          # Get time elapsed
 
                     if ok:
-                        x, y = get_center(bbox)                         # Get center of bounding box
-                        export_csv(name, tracker_type, t, x, y)         # Export success to CSV
-                        # draw_box(frame, bbox)                           # Draw box
+                        x, y = get_center(bbox)                                                 # Get center of bounding box
+                        export_csv(name, tracker_type, t, x, y, fps)                            # Export success to CSV
+                        draw_box(frame, bbox)                                                   # Draw box
                     else:
-                        export_csv(name, tracker_type, t, -1, -1)       # Export failure to CSV
-                        # draw_fail(frame)                                # Write failure alert
+                        export_csv(name, tracker_type, t, -1, -1, 0)                            # Export failure to CSV
+                        draw_fail(frame)                                                        # Write failure alert
 
-                    # display_tracker(frame, tracker_type, fps)           # Display tracker on image
-
-
-
-# (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+                    count = display_tracker(frame, frame_num, tracker_type, fps, name, count)   # Create image
+                    frame_num += 1                                                              # Increment frame number
 
 if __name__ == '__main__' :
     main()
-
-
-    # directory = "../video"
-    #
-    # # Set up tracker types
-    # tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-    #
-    # # Implement trackers on all videos in the videos/ directory
-    # for filename in os.listdir(directory):
-    #     if filename.endswith(".mp4"):
-    #         print filename
-    #
-    #         # Add new data directory if it doesn't exist
-    #         name = filename[:-4]    # Remove the .mp4 from the filename
-    #         if not os.path.exists("../data/"+name):
-    #             os.makedirs("../data/"+name)
-    #             print name,"folder added"
-    #
-    #         # Open video
-    #         video = cv2.VideoCapture(os.path.join(directory, filename))
-    #
-    #         # Exit if video not opened.
-    #         if not video.isOpened():
-    #             print "Could not open video"
-    #             sys.exit()
-    #
-    #         # Read first frame.
-    #         ok, frame = video.read()
-    #         if not ok:
-    #             print 'Cannot read video file'
-    #             sys.exit()
-    #
-    #         # Set bounding box for tracking
-    #         bboxOG = cv2.selectROI(frame, False)
-    #
-    #         for i in range(2):
-    #             j = 0
-    #             bbox = bboxOG
-    #             tracker_type = tracker_types[i]
-    #             if tracker_type == 'BOOSTING':
-    #                 tracker = cv2.TrackerBoosting_create()
-    #             if tracker_type == 'MIL':
-    #                 tracker = cv2.TrackerMIL_create()
-    #             if tracker_type == 'KCF':
-    #                 tracker = cv2.TrackerKCF_create()
-    #             if tracker_type == 'TLD':
-    #                 tracker = cv2.TrackerTLD_create()
-    #             if tracker_type == 'MEDIANFLOW':
-    #                 tracker = cv2.TrackerMedianFlow_create()
-    #             if tracker_type == 'GOTURN':
-    #                 tracker = cv2.TrackerGOTURN_create()
-    #
-    #             video = cv2.VideoCapture(os.path.join(directory, filename))
-    #
-    #             # Read first frame.
-    #             ok, frame = video.read()
-    #
-    #             # Initialize tracker with first frame and bounding box
-    #             ok = tracker.init(frame, bbox)
-    #             time = cv2.getTickCount()
-    #
-    #             while True:
-    #                 # Read a new frame
-    #                 ok, frame = video.read()
-    #                 if not ok:
-    #                     break
-    #
-    #                 # Start timer
-    #                 timer = cv2.getTickCount()
-    #
-    #                 # Update tracker
-    #                 ok, bbox = tracker.update(frame)
-    #
-    #                 # Calculate Frames per second (FPS)
-    #                 fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-    #                 t = int(((cv2.getTickCount() - time)/cv2.getTickFrequency())*1000)
-    #
-    #                 # Draw bounding box
-    #                 if ok:
-    #                     # Tracking success
-    #                     # p1 = (int(bbox[0]), int(bbox[1]))
-    #                     # p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    #                     # cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-    #                     x = int(bbox[0]+bbox[2]/2)
-    #                     y = int(bbox[1]+bbox[3]/2)
-    #
-    #                     # Add to CSV
-    #                     with open("../data/"+name+"/"+tracker_type+".csv", "a") as csvfile:
-    #                         filewriter = csv.writer(csvfile, delimiter = ",", quotechar = "|", quoting = csv.QUOTE_MINIMAL)
-    #                         filewriter.writerow([t, x, y])
-    #
-    #                 else :
-    #                     # Tracking failure
-    #                     # cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-    #
-    #                     # Add to CSV
-    #                     with open(tracker_type+".csv", "a") as csvfile:
-    #                         filewriter = csv.writer(csvfile, delimiter = ",", quotechar = "|", quoting = csv.QUOTE_MINIMAL)
-    #                         filewriter.writerow([t, -1, -1])
-    #
-    #                 # # Display tracker type on frame
-    #                 # cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-    #                 #
-    #                 # # Display FPS on frame
-    #                 # cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-    #                 #
-    #                 # # Display result
-    #                 # cv2.imshow("Tracking", frame)
-    #                 #
-    #                 # # Exit if ESC pressed
-    #                 # k = cv2.waitKey(3) & 0xff
-    #                 # if k == 27 : break
-    #
-    #         continue
-    #     else:
-    #         continue
--- tracker.m
+++ tracker.m
@@ -12,18 +12,32 @@ for i = 3:length(files)
     
     %% Plot position of bounding box
     figure('Name', files(i).name)
+    subplot(2,1,1)
     hold on;
-    plot(boosting(:,2), boosting(:,3))
-    plot(kcf(:,2), kcf(:,3))
-    plot(medianflow(:,2), medianflow(:,3))
-    plot(mil(:,2), mil(:,3))
-    plot(tld(:,2), tld(:,3))
+    plot(1:length(boosting), boosting(:,3))
+    plot(1:length(kcf), kcf(:,3))
+    plot(1:length(medianflow), medianflow(:,3))
+    plot(1:length(mil), mil(:,3))
+    plot(1:length(tld), tld(:,3))
+    legend("Boosting", "KCF", "MedianFlow", "MIL", "TLD")
+    title("Y Position over Time");
+    ylabel("Position (px)");
+    xlabel("Time (ms)");
     hold off;
+    
+    subplot(2,1,2)
+    hold on;
+    plot(1:length(boosting), boosting(:,2))
+    plot(1:length(kcf), kcf(:,2))
+    plot(1:length(medianflow), medianflow(:,2))
+    plot(1:length(mil), mil(:,2))
+    plot(1:length(tld), tld(:,2))
+    legend("Boosting", "KCF", "MedianFlow", "MIL", "TLD")
+    title("X Position over Time");
+    ylabel("Position (px)");
+    xlabel("Time (ms)");
+    hold off;
+    
 end
-
-
-
-
-
 
 end
